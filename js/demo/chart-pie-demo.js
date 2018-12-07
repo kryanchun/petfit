e7ff2612560b70bc47c73ca7b8ab7c2e667eec1f
Ryan Chun <ryan.chun@columbiaspectator.com>
2018-12-07 11:33:46 -0500

    chart fix

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,10 +7,10 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
+    labels: ["Normal", "concerned", "emergency"],
     datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+      data: [86, 8, 1],
+      backgroundColor: [ '#28a745', '#ffc107', '#dc3545'],
     }],
   },
 });
